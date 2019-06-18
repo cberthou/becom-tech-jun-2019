e2c9d2a32a796190d85fb138a95e8f0e999561d1
Clément Berthou <clement.berthou.ext@francetv.fr>
2019-06-18 18:58:24 +0200

    Updates

---
+++ precache-manifest.26b598fd68681bedca77702fa1f26911.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4236b8bd4d7697e65bf9e27454c641d6",
+    "url": "/becom-tech-jun-2019/index.html"
+  },
+  {
+    "revision": "9902c340ce6d33cbb985",
+    "url": "/becom-tech-jun-2019/static/css/2.da5487dc.chunk.css"
+  },
+  {
+    "revision": "174e5e6d30b569c78a3f",
+    "url": "/becom-tech-jun-2019/static/css/main.4492c7ab.chunk.css"
+  },
+  {
+    "revision": "9902c340ce6d33cbb985",
+    "url": "/becom-tech-jun-2019/static/js/2.2be3c750.chunk.js"
+  },
+  {
+    "revision": "174e5e6d30b569c78a3f",
+    "url": "/becom-tech-jun-2019/static/js/main.9a5aaeb4.chunk.js"
+  },
+  {
+    "revision": "c5831afbe559c5f524b7",
+    "url": "/becom-tech-jun-2019/static/js/runtime~main.24e52883.js"
+  },
+  {
+    "revision": "91295fa87df918411b49b7531da5d558",
+    "url": "/becom-tech-jun-2019/static/media/league-gothic.91295fa8.ttf"
+  },
+  {
+    "revision": "9900a4643cc63c5d8f969d2196f72572",
+    "url": "/becom-tech-jun-2019/static/media/league-gothic.9900a464.eot"
+  },
+  {
+    "revision": "cd382dc8a9d6317864b5810a320effc5",
+    "url": "/becom-tech-jun-2019/static/media/league-gothic.cd382dc8.woff"
+  }
+]);
